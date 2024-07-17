5cb0e9f6cbc9f5c6b6a78a4ea17aaa66ffc0f87d
Matheus Ribeiro <matheus.ribeiro.mrs762@gmail.com>
2024-07-17 20:25:12 -0300

    feat: add bootcamp challenge initial structure

---
+++ Desafios/bootcamp/src/br/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,25 @@
+package br.com.dio.desafio.dominio;
+
+public class Curso extends Conteudo {
+    private int cargaHoraria;
+
+    public Curso() {}
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso [cargaHoraria=" + cargaHoraria + ", descricao=" + getDescricao() + ", titulos=" + getTitulo() + "]";
+    }
+}
